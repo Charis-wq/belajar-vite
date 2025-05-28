63310b49764b1e7e749a8dff5a4b662a152bf9d9
Abdul Charis <konfoy7@gmail.com>
2025-05-29 00:07:42 +0700

    mempush commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import {buildErrorMessage, defineConfig} from "vite";
+
+export default defineConfig({
+    build:{
+        outDir: "production",
+        rollupOptions: {
+            input: {
+                index: "index.html",
+                blog: "blog.html",
+                contact: "other/contact.html"
+
+            }
+        }
+    },
+    server:{
+        port: 3000
+    }
+
+});
